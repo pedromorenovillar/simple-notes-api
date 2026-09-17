736e0ceecd76a716a3086e2a406dabecb64a5fa0
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-09-17 15:07:00 +0200

    test: add health test

---
+++ src/tests/health.test.ts
@@ -0,0 +1,15 @@
+// ¿Está Vitest configurado correctamente?
+// ¿Está Supertest configurado correctamente?
+// ¿Importo app correctamente?
+
+import { describe, it, expect } from "vitest";
+import request from "supertest";
+
+import app from "../app";
+
+describe("GET /health", () => {
+  it("returns 200", async () => {
+    const response = await request(app).get("/health");
+    expect(response.status).toBe(200);
+  });
+});
